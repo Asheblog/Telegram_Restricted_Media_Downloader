fe67388ea23daf93a3cceb0e3d44cfeacdeb6221
Asheblog <402947780@qq.com>
2026-06-25 12:49:36 +0800

    Bump version to v0.1.1

--- module/__init__.py
+++ module/__init__.py
@@ -50,9 +50,9 @@ LOG_TIME_FORMAT = '[%Y-%m-%d %H:%M:%S]'
 console = Console(log_path=False, log_time_format=LOG_TIME_FORMAT)
 SLEEP_THRESHOLD = 60
 AUTHOR = 'Gentlesprite'
-__version__ = '0.0.9'
+__version__ = '0.1.1'
 __license__ = 'MIT License'
-__update_date__ = '2026/06/24 11:32:44'
+__update_date__ = '2026/06/25 12:48:00'
 __copyright__ = f'Copyright (C) 2024-{__update_date__[:4]} {AUTHOR} <https://github.com/Gentlesprite>'
 SOFTWARE_FULL_NAME = 'Telegram Restricted Media Downloader'
 SOFTWARE_SHORT_NAME = 'TRMD'
